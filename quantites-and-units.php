b7b3811700dd2b24f855a29e1be26ca587f2e0dd
Nick Verwymeren <me@nickv.codes>
2016-12-08 20:43:51 -0600

    version bump

--- quantites-and-units.php
+++ quantites-and-units.php
@@ -3,7 +3,7 @@
 Plugin Name: Quantities and Units for WooCommerce
 Plugin URI: https://wordpress.org/plugins/quantities-and-units-for-woocommerce/
 Description: Easily require your customers to buy a minimum / maximum / incremental amount of products to continue with their checkout.
-Version: 1.0.12
+Version: 1.0.13
 Author: Nicholas Verwymeren
 Author URI: https://www.nickv.codes
 */ 
